06878a79f96bf6795710c98d820cb3ce5867db36
BuildTools <unconfigured@null.spigotmc.org>
2016-06-14 23:45:23 +1000

    Beginning
    Added Project horse to repo

---
+++ Assets/Scripts/playerClick.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerClick : MonoBehaviour {
+    public UnityEngine.UI.Image image;
+    public UnityEngine.UI.Text m_scoreText;
+    public int score;
+    public int scoreIncrement;
+
+	// Use this for initialization
+	void Start () {
+        m_scoreText.text = "Kappas: " + score;
+        score = 0;
+        scoreIncrement = 1;
+        image = GetComponent<UnityEngine.UI.Image>();
+        image.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_scoreText.text = "Kappas: " + score;
+	}
+
+	public void AddScore()
+    {
+        score += scoreIncrement;
+       //image.enabled;
+       //if (Random.Range(0,17) == 17)
+       // {
+       //     image.enabled = true;
+       // }
+    }
+
+   
+}
---
+++ Assets/Scripts/upgradeManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class upgradeManager : MonoBehaviour {
+
+    public playerClick m_pClick;
+    public UnityEngine.UI.Text itemInfo;
+    public int m_cost;
+    public int m_count;
+    public int clickPower;
+    public string m_name;
+    private float _newCost;
+
+    void Update()
+    {
+        itemInfo.text = m_name + "\nCost: " + m_cost + "\nPower: " + clickPower;
+    }
+
+    public void PurchaseUpgrade()
+    {
+        if (m_pClick.score >= m_cost)
+        {
+            ++m_count;
+            m_pClick.score -= m_cost;
+            m_pClick.scoreIncrement += clickPower;
+            m_cost = (int)Mathf.Round(m_cost * 1.15f);
+            _newCost = Mathf.Pow(m_cost, m_cost);
+        }
+    }
+}
